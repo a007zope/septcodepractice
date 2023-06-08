7ed8d1fd07c77116653989dc8b75a774cbafcf8f
vzopad <aditya.zope@pearson.com>
2023-06-08 10:38:53 +0530

    cricinfo

--- Sessions/src/main/java/SeleniumSessionsByNaveen/CricInfo.java
+++ Sessions/src/main/java/SeleniumSessionsByNaveen/CricInfo.java
@@ -22,38 +22,43 @@ public class CricInfo {
 		co.addArguments("--remote-allow-origins=*");
 		driver = new ChromeDriver(co);
 		driver.get("https://www.espncricinfo.com/series/australia-a-in-new-zealand-2023-1365973/new-zealand-a-vs-australia-a-1st-unofficial-test-1365976/full-scorecard");
-		System.out.println(getWicketTakerName("Xavier Bartlett"));
-		System.out.println(getWicketTakerName("Campbell Kellaway"));
-		System.out.println(getWicketTakerName("Nathan McSweeney"));
-		System.out.println(getWicketTakerName("Tim Ward"));
-		
-		 ArrayList<String> scoreCard =getScoreCard("Tim Ward");
-		 System.out.println(scoreCard);
-	
+		//		System.out.println(getWicketTakerName("Xavier Bartlett"));
+		//		System.out.println(getWicketTakerName("Campbell Kellaway"));
+		//		System.out.println(getWicketTakerName("Nathan McSweeney"));
+		//		System.out.println(getWicketTakerName("Tim Ward"));
+		//		
+		ArrayList<String> scoreCard =getScoreCard("Nathan McSweeney");
+		System.out.println(scoreCard);
+
 	}
 
 	public static String getWicketTakerName(String batsmanName)
 	{
 		return driver.findElement(By.xpath("(//span[text()='"+batsmanName+"']/ancestor::td/following::td)[position()=1]")).getText();
 	}
-	
+
 	public static ArrayList<String> getScoreCard(String name)
 	{
-		 
-		 ArrayList<String> scoreCardList = new ArrayList<String>();
-		
+
+		ArrayList<String> scoreCardList = new ArrayList<String>();
+
+		//		List<WebElement> webeleList	=driver.findElements(By.xpath("//span[text()='"+name+"']//following::td[contains(@class,'ds-w-0 ds-whitespace-nowrap ds-min-w-max ds-text-right')]"));
+		//          for(WebElement e:webeleList)
+		//          {
+		//        	  scoreCardList.add(e.getAccessibleName());
+		//          }
+
 		for(int i =1;i<=6;i++)
 		{
-		List<WebElement> scoreList = driver.findElements("(//span[text()='"+name+"']/ancestor::td/following::td/following::td/following::td)[i]");
-		
-		    for(WebElement e: scoreList)
-		    {
-		    	scoreCardList.add(e.getText());
-		    }
+
+			String scoreno=driver.findElement(By.xpath("(//span[text()='"+name+"']//following::td[contains(@class,'ds-w-0 ds-whitespace-nowrap ds-min-w-max ds-text-right')])["+i+"]")).getAccessibleName();
+			scoreCardList.add(scoreno);
+
 		}
+
 		return scoreCardList;
 	}
 }
-	
+
 
 
