eaa5d6c55952ccf7f1e6759eb19bcf578f666e6f
vzopad <aditya.zope@pearson.com>
2023-06-17 18:24:29 +0530

    latestcode

---
+++ Sessions/InterviewProgramsPractice/switchForInt.java
@@ -0,0 +1,83 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.Select;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class switchForInt {
+	
+	static WebDriver driver;
+	
+	public static void main(String[] args) {
+		/*
+		 * int variable = 6; switch(variable) { case'1':System.out.println(); break;
+		 * case'2':System.out.println(); break; case'3':System.out.println(); break;
+		 * case'6':System.out.println(); break;
+		 * default:System.out.println("execute default statement");
+		 * 
+		 * }
+		 */
+		
+	//	String car ="m";
+//		
+//		switch(car)
+//		{
+//		case "m":System.out.println("BMW");
+//		break;
+//		case "a":System.out.println("Audii");
+//		break;
+//		case "M":System.out.println("Maruti");
+//		break;
+//		case "z":System.out.println("Zen");
+//		break;
+//		case "T":System.out.println("Truck");
+//		break;
+//		case "A":System.out.println("Aeroplane");
+//		break;
+//		default:System.out.println("please select car");
+//		
+//		}
+		WebDriverManager.chromedriver().setup();
+		ChromeOptions co = new ChromeOptions();
+		co.addArguments("--remote-allow-origins=*"); 
+		driver = new ChromeDriver(co);
+		driver.get("https://www.facebook.com/reg");
+		
+		 WebElement day = driver.findElement(By.id("day"));
+		  WebElement month = driver.findElement(By.id("month"));
+		WebElement year = driver.findElement(By.id("year"));
+		
+		
+		selectElement("value",day,"19");
+		selectElement("value",month,"4");
+		selectElement("value",year,"2015");
+		
+	}
+	
+	public static void selectElement(String locatorvalue,WebElement element,String value)
+	{
+		Select s = new Select(element);
+		switch(locatorvalue)
+		{
+		case"visibleText" :
+			s.selectByVisibleText(value);
+			break;
+			
+		case "index":
+			s.selectByIndex(Integer.parseInt(value));
+			break;
+			
+		case"value":
+		   s.selectByValue(value);
+		   break;
+		   
+		default:
+			System.out.println("default selection");
+		}
+	}
+	
+
+}
---
+++ Sessions/InterviewProgramsPractice/switchcasestatementexample.java
@@ -0,0 +1,25 @@
+
+public class switchcasestatementexample {
+
+
+	public static void main(String[] args) {
+		char c ='r';
+		switch(c)
+		{
+
+		case'q':
+			System.out.println("red color");
+			System.out.println("red font");
+			break;
+		case'r':
+			System.out.println("white color");
+			System.out.println("white font");
+			break;
+
+		default:
+			System.out.println("please enter valid combination");
+
+		}
+	}
+
+}
