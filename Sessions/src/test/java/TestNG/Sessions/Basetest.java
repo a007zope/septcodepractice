4503f0dfe5e198731ebbd2e877c842b43dcb5434
vzopad <aditya.zope@pearson.com>
2023-07-07 18:27:16 +0530

    Latest TestNG update

--- Sessions/src/test/java/TestNG/Sessions/Basetest.java
+++ Sessions/src/test/java/TestNG/Sessions/Basetest.java
@@ -3,6 +3,8 @@ package TestNG.Sessions;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
@@ -10,19 +12,36 @@ import org.testng.annotations.Parameters;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Basetest {
-	
+
 	static WebDriver driver;
-	@Parameters({"url"})
+	@Parameters({"url","browser"})
 	@BeforeTest
-	public void setup(String url)
+	public void setup(String url,String browser)
 	{
-		WebDriverManager.chromedriver().setup();
-		ChromeOptions co = new ChromeOptions();
-		co.addArguments("--remote-allow-origins=*");
-		driver = new ChromeDriver(co);
+		if(browser.equalsIgnoreCase("chrome"))
+		{
+			WebDriverManager.chromedriver().setup();
+			ChromeOptions co = new ChromeOptions();
+			co.addArguments("--remote-allow-origins=*");
+			driver = new ChromeDriver(co);	
+		}
+		else if(browser.equalsIgnoreCase("firefox"))
+		{
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+		}
+		else if(browser.equalsIgnoreCase("edge"))
+		{
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+		}
+		else
+		{
+			System.out.println("please enter correct browserName");
+		}
 		driver.get(url);
 	}
-	
+
 	@AfterTest
 	public void quit()
 	{
