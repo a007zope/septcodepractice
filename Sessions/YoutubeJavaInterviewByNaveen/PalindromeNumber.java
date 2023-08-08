97a675562de57cf6e8241ee272ce379f76f887de
vzopad <aditya.zope@pearson.com>
2023-08-08 22:54:17 +0530

    latestcodechangeForPalindromenumberandString

--- Sessions/YoutubeJavaInterviewByNaveen/PalindromeNumber.java
+++ Sessions/YoutubeJavaInterviewByNaveen/PalindromeNumber.java
@@ -1,27 +1,27 @@
 
 public class PalindromeNumber {
-	
+
 
 	/*
 	 * Palindrome number is the number which when reversed comes out to be the same number
 	 * eg : 151 454 34543 161 78987
 	 */
-	
-	
+
+
 	public static void isPalindromeNumber(int num)
 	{
 		int r =0;
 		int sum =0;
 		int t;
 		t = num;
-		
+
 		while(num>0)
 		{
 			r = num%10; // get the remainder
 			sum = (sum*10) + r;
 			num = num/10;
 		}
-		
+
 		if(t==sum)
 		{
 			System.out.println("Palindrome Number");
@@ -30,18 +30,18 @@ public class PalindromeNumber {
 		{
 			System.out.println("Not a Palindrome Number");
 		}
-		
-		
+
+
 	}
-	
-	
+
+
 	public static void main(String[] args) {
-		
-		
-  isPalindromeNumber(151);
-  isPalindromeNumber(152);
-		
-		
+
+
+		isPalindromeNumber(151);
+		isPalindromeNumber(152);
+
+
 	}
 
 }
--- Sessions/YoutubeJavaInterviewByNaveen/PrimeNumber.java
+++ Sessions/YoutubeJavaInterviewByNaveen/PrimeNumber.java
@@ -30,14 +30,12 @@ public class PrimeNumber {
 				System.out.println(i + " ");
 			}
 		}
-
 	}
-
 
 	public static void main(String[] args) {
 
 		// prime number is special number which can be divided by itself or 1.
-
+		
 		System.out.println("2 is prime number" +" "+ isPrimeNumber(1));
 		System.out.println("4 is prime number" +" "+isPrimeNumber(4));
 		System.out.println("5 is prime number" +" "+ isPrimeNumber(5));
