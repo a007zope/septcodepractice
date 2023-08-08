edd5295584b33a3832ff9538c8e0b8b51b889761
vzopad <aditya.zope@pearson.com>
2023-08-08 14:48:33 +0530

    latestcodechangeinprimenumber

--- Sessions/YoutubeJavaInterviewByNaveen/PrimeNumber.java
+++ Sessions/YoutubeJavaInterviewByNaveen/PrimeNumber.java
@@ -9,7 +9,6 @@ public class PrimeNumber {
 		if(num<=1)
 		{
 			return false;
-
 		}
 
 		for(int i =2;i<num;i++)
@@ -20,6 +19,17 @@ public class PrimeNumber {
 			}
 		}
 		return true;
+	}
+
+	public static void getPrimeNumbers(int num)
+	{
+		for(int i =2;i<=num;i++)
+		{
+			if(isPrimeNumber(i))
+			{
+				System.out.println(i + " ");
+			}
+		}
 
 	}
 
@@ -28,7 +38,12 @@ public class PrimeNumber {
 
 		// prime number is special number which can be divided by itself or 1.
 
-		System.out.println("2 is prime number"+ isPrimeNumber(1));
+		System.out.println("2 is prime number" +" "+ isPrimeNumber(1));
+		System.out.println("4 is prime number" +" "+isPrimeNumber(4));
+		System.out.println("5 is prime number" +" "+ isPrimeNumber(5));
+		System.out.println("3 is prime number" +" "+ isPrimeNumber(3));
+		getPrimeNumbers(7);
+		getPrimeNumbers(25);
 
 
 
